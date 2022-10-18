09392b3df129a1d5d7ada74feaaa70d44e0cba08
Elijah Patric <foreverelijah@gmail.com>
2022-10-18 15:59:47 -0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Elijah on 10/18/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
